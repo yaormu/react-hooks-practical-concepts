e7ffdf8b01f804cc24cf6fb5d5172d0b609184aa
yaormu <58757141+yaormu@users.noreply.github.com>
2022-04-12 22:11:29 -0500

    actualizar a react 18

--- 1-todo-app/src/index.js
+++ 1-todo-app/src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -6,3 +7,14 @@ ReactDOM.render(
     <App/>,
     document.querySelector("#root")
 );
+*/
+
+//React 18+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const divRoot = document.querySelector('#app');
+const root = createRoot(divRoot);
+
+root.render( <App tab="home"/>);
